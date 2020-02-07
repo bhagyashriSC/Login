e6a5d0d899675bd91b72aa2258f0ee384d0295cd
unknown <bhagyashree.ssc@gmail.com>
2020-02-07 12:34:06 +0530

    app file

--- src/App.js
+++ src/App.js
@@ -12,23 +12,20 @@ class App extends React.Component{
       
         <BrowserRouter>
         <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav">
-    
-      <li> <Link to="/">Login</Link></li>
-      <Link to="/logout">Logout</Link>
-      
-    </ul>
-  </div>
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <Link class="navbar-brand" to="/">Logo</Link>
+          </div>
+          <div class="nav navbar-nav">          
+             <Link to="/">Home</Link>          
+          </div>
+        </div>
 </nav>
 
       
       <hr/>
       <Route exact path="/" component={Login} />
-      <Route exact path="/logout" component={Logout} /> 
+      
       </BrowserRouter>
        
         </div>
